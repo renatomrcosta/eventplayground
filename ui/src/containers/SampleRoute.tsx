6cbea5ec40ff0f4a1c4ed38a90f8ffe9d6c5b8fc
Renato Costa <renato.rezende.costa@gmail.com>
2021-02-19 21:37:00 +0100

    fetching values from API

--- ui/src/containers/SampleRoute.tsx
+++ ui/src/containers/SampleRoute.tsx
@@ -1,32 +1,14 @@
-import React, { useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { EventCard } from "../components/EventCard";
-
-interface EventData {
-  id: string;
-  title: string;
-  description: string;
-  images: string[];
-}
-
+import * as EventService from "../service/EventService";
+import { EventData } from "../service/EventService";
 export const SampleRoute: React.FC = () => {
-  const getData = useCallback(() => {
-    return [
-      {
-        id: "123",
-        title: "Title1",
-        description: "desc1",
-        images: [],
-      },
-      {
-        id: "45",
-        title: "Title2",
-        description: "desc2",
-        images: [],
-      },
-    ];
+  const [events, setEvents] = useState<EventData[]>([]);
+
+  useEffect(() => {
+    EventService.getEventData().then((data) => setEvents(data));
   }, []);
-  // Given a collection of items, render the event cards
-  const events: EventData[] = getData();
+
   return (
     <>
       {events.map((event) => {
