04aa6308c754e468e6b4c362c3a1377fdf4ff602
Renato Costa <renato.rezende.costa@gmail.com>
2021-02-19 21:03:43 +0100

    prototype route for event data

--- ui/src/containers/SampleRoute.tsx
+++ ui/src/containers/SampleRoute.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { EventCard } from "../components/EventCard";
 
+interface EventData {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+}
+
 export const SampleRoute: React.FC = () => {
+  const getData = useCallback(() => {
+    return [
+      {
+        id: "123",
+        title: "Title1",
+        description: "desc1",
+        images: [],
+      },
+      {
+        id: "45",
+        title: "Title2",
+        description: "desc2",
+        images: [],
+      },
+    ];
+  }, []);
+  // Given a collection of items, render the event cards
+  const events: EventData[] = getData();
   return (
-    <EventCard
-      title="Title!"
-      description="Description"
-      onClick={() => alert("banana")}
-    />
+    <>
+      {events.map((event) => {
+        return (
+          <EventCard
+            key={event.id}
+            title={event.title}
+            description={event.description}
+            onClick={() => alert(event.id)}
+          />
+        );
+      })}
+    </>
   );
 };
